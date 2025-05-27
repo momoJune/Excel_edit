33adc551c86af2b648666f69a5cf9fd189362514
kimdonghyun <kimdongh7277@naver.com>
2025-05-27 16:25:25 +0900

    [종합포털][김동현] 커밋 테스트

---
+++ src/main/java/org/example/excel_edit/controller/TestController.java
@@ -0,0 +1,21 @@
+package org.example.excel_edit.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class TestController {
+
+    @Autowired FileController fileController;
+
+    @GetMapping("/")
+    public String index() {
+
+        // 1차 커밋
+
+        return "index";
+    }
+
+}
